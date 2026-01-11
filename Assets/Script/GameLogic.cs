bb4e253d30ec5d36265cdef008d02631ac980e13
Honey-Badger-407 <kartikeychoudhary@proton.me>
2026-01-11 20:13:51 +0530

    final Completion Commit

--- Assets/Script/GameLogic.cs
+++ Assets/Script/GameLogic.cs
@@ -3,31 +3,33 @@ using System.Collections.Generic;
 using Cainos.PixelArtTopDown_Basic;
 using UnityEngine;
 using TMPro;
+using Unity.Mathematics;
 
 
 public class GameLogic : MonoBehaviour
 {
     public List<string> Pillars;
+    public List<GameObject> PillarsObjects;
     public List<GameObject> Glow;
     private int Score = 0;
     public string Selected_Pillar;
     private int pillarAdd;
-    public float selectionWindow = 2f;
+    public float selectionWindow = 4f;
     private bool canSelect = false;
     private Coroutine ActionWindow;
-    private SpriteRenderer SR;
     private Queue<float> reactionTimeQueue;
     private const int Queue_Size =10;
     private float roundStartTime;
-    private float lastReactionTime,Accuracy;
+    private float lastReactionTime,Accuracy,MaxWindowSize = 4f,MinWindowSize=0.16f;
     private int WrongClicks=0,ClickCount=0;
+    private Vector3 obj,RandomPosition;
     [SerializeField] private TMP_Text scoreText;
     [SerializeField] private TMP_Text wrongClicksText;
     [SerializeField] private TMP_Text AccuracyText;
     private OddRule currentRule;
     enum OddRule
     {
-        GlowOnly,RotationOnly
+        RotatePillar,movePillar
     }
     void Start()
     {
@@ -45,6 +47,7 @@ public class GameLogic : MonoBehaviour
     }
     IEnumerator SelectionWindow(float duration)
     {
+        Debug.Log(duration);
         canSelect = true;
         roundStartTime =Time.time;
         float elapsedTime=0f;
@@ -52,10 +55,33 @@ public class GameLogic : MonoBehaviour
         {
             elapsedTime +=Time.deltaTime;
             float normalizedTime = Mathf.Clamp01(elapsedTime / duration);
-            Color c = SR.color;
-            c.a = Mathf.Lerp(0f,1f,normalizedTime);
-            SR.color = c;
-            yield return null;
+            for(int i=0;i<Glow.Count;i++)
+            {
+                SpriteRenderer sr;
+                if(Glow[i]==Glow[pillarAdd])
+                    continue;
+
+                normalizedTime = Mathf.Clamp01(elapsedTime / duration);
+                sr = Glow[i].GetComponent<SpriteRenderer>();
+                Color c = sr.color;
+                c.a = math.lerp(1f,0f,normalizedTime);
+                sr.color = c;
+                Glow[i].GetComponent<SpriteColorAnimation>().enabled = false;
+            }
+            switch (currentRule)
+            {
+                case OddRule.RotatePillar:
+                {
+                    Quaternion startRotation = Quaternion.Euler(0f,0f,0f);
+                    Quaternion targetRotation = Quaternion.Euler(0f, 0f, 90f);
+                    PillarsObjects[pillarAdd].transform.rotation = Quaternion.Lerp(startRotation, targetRotation, normalizedTime);
+                    break;
+                }                
+                case OddRule.movePillar:
+                    PillarsObjects[pillarAdd].transform.position = Vector3.Lerp(obj, RandomPosition, normalizedTime);
+                    break;
+            }
+                yield return null;
         }   
         if(canSelect)
         {
@@ -64,6 +90,7 @@ public class GameLogic : MonoBehaviour
             WrongClicks++;
             AddReactionTime(selectionWindow);
             AdjustDifficulty();
+            Reset();
             SetColor();
             SelectPillar();
         }
@@ -71,11 +98,11 @@ public class GameLogic : MonoBehaviour
     }
     void SelectPillar()
 {
-    pillarAdd = Random.Range(0, Pillars.Count);
+    pillarAdd = UnityEngine.Random.Range(0, Pillars.Count);
     Selected_Pillar = Pillars[pillarAdd];
-    currentRule = (OddRule)Random.Range(0, 2);
+    currentRule = (OddRule)UnityEngine.Random.Range(0, 2);
     
-
+    obj = PillarsObjects[pillarAdd].transform.position;
     ApplyRule();                     
 
     UpdateUI();
@@ -93,7 +120,7 @@ public class GameLogic : MonoBehaviour
         canSelect = false;
         lastReactionTime = Time.time - roundStartTime;
         AddReactionTime(lastReactionTime);
-        Debug.Log(Player_Selected);
+        Reset();
         CheckSelection(Player_Selected);
     }
 
@@ -127,9 +154,9 @@ public class GameLogic : MonoBehaviour
         {
             sr = Glow[i].GetComponent<SpriteRenderer>();
             Color c = sr.color;
-            c.a = 0f;
+            c.a = 1f;
             sr.color = c;
-            Glow[i].GetComponent<SpriteColorAnimation>().enabled = false;
+            Glow[i].GetComponent<SpriteColorAnimation>().enabled = true;
         }
     }
     void AddReactionTime(float reactionTime)
@@ -165,12 +192,11 @@ public class GameLogic : MonoBehaviour
 
         switch (currentRule)
         {
-            case OddRule.GlowOnly:
-                ApplyGlowRule();
+            case OddRule.RotatePillar:
+                Applyrotation();
                 break;
-
-            case OddRule.RotationOnly:
-                ApplyRotationRule();
+            case OddRule.movePillar:
+                ApplyMovement();
                 break;
         }
     }
@@ -178,22 +204,28 @@ public class GameLogic : MonoBehaviour
     {
         float avgReaction = GetAverageReactionTime();
 
-        float normalized = Mathf.Clamp01(avgReaction / 2f);
+        float normalized = Mathf.Clamp01(avgReaction / MaxWindowSize);
 
-        float targetWindow = Mathf.Lerp(2f, 0.6f, normalized);
+        float targetWindow = Mathf.Lerp(MinWindowSize, MaxWindowSize, normalized);
 
-        selectionWindow = Mathf.Lerp(selectionWindow, targetWindow, 0.1f);
+        selectionWindow = Mathf.Lerp(selectionWindow, targetWindow, 0.5f);
     }
 
 
-    void ApplyGlowRule()
+    void Applyrotation()
     {
-        SR = Glow[pillarAdd].GetComponent<SpriteRenderer>();
+    PillarsObjects[pillarAdd].transform.rotation = Quaternion.Euler(0f, 0f, 0f);
     }
-    void ApplyRotationRule()
+    void ApplyMovement()
     {
-        Glow[pillarAdd].transform.rotation = Quaternion.identity;
-        Glow[pillarAdd].transform.rotation = Quaternion.Euler(0f, 0f, 45f);
-        SR = Glow[pillarAdd].GetComponent<SpriteRenderer>();
+        RandomPosition = new Vector3(
+            UnityEngine.Random.Range(obj.x-10, obj.x+10),
+            UnityEngine.Random.Range(obj.y-10,obj.y+10 ),0f);
+        
+    }
+    void Reset()
+    {
+        PillarsObjects[pillarAdd].transform.rotation=Quaternion.Euler(0f,0f,0f);
+        PillarsObjects[pillarAdd].transform.position= obj;
     }
 }
